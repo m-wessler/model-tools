fffe5a6fd0b189b6ecfe45a501b729dd49734427
m-wessler <m.wessler@utah.edu>
2020-09-29 14:44:45 -0600

    updated subset script

--- era5/subset_era5.py
+++ era5/subset_era5.py
@@ -27,6 +27,7 @@ def ncks_subset(i, fl, gridtype, wait=True):
         if mem_avail > (mem_need*2):
             print('Processing {}/{} REQ:{}GB AVAIL:{}GB [{}]'.format(i+1, len(fl), mem_need, mem_avail, f))
             wait = False
+            # print(cmd)
             return call(cmd, shell=True)
             
         else:
@@ -35,52 +36,55 @@ def ncks_subset(i, fl, gridtype, wait=True):
 
 if __name__ == '__main__':
 
-    griddtype = sys.argv[1]
+    gridtype = sys.argv[1]
 
-    isodir = '/scratch/general/lustre/u1070830/era5_iso/'
-    sfcdir = '/scratch/general/lustre/u1070830/era5_sfc/'
-    model_dir = isodir if griddtype == 'isobaric' else sfcdir
+    isodir = '/scratch/general/lustre/u1070830/era5_temp/'
+    sfcdir = '/scratch/general/lustre/u1070830/era5_temp/'
+    model_dir = isodir if gridtype == 'isobaric' else sfcdir
 
     dirlist = np.array(glob(model_dir + '*'))
     dirlist = dirlist[np.argsort(dirlist)]
-
+    
     for d in dirlist:
         print('pwd: %s'%d)
         
-        flist = glob(d + '/*.nc')
+        gridtype_spec = '.pl.' if gridtype == 'isobaric' else '.sfc.'
+        flist = sorted(glob(d + '/*%s*.nc'%gridtype_spec))
         flist = [f for f in flist if '.WE.nc' not in f]
+        
+        ncks_subset_mp = partial(ncks_subset, fl=flist, gridtype=gridtype, wait=True)        
+        # print(sorted([f.split('_')[3].split('.')[0] for f in flist]))
 
         if len(flist) > 0:
             print(len(flist), d)
 
             # Add a failsafe that loads flist[0] and displays the lat/lon bounds
             # Then yes/no prompt for user to continue with this year and var set
-            # ncks_subset_mp = partial(ncks_subset, fl=flist, gridtype=griddtype)
-
+            # ncks_subset_mp = partial(ncks_subset, fl=flist, gridtype=gridtype)
             ncks_subset_mp(0)
-            f = glob(d + '/*.WE.nc')[0]
-            sample_post = xr.open_dataset(f)
+            fi = glob(d + '/*.WE.nc')[0]
+            sample_post = xr.open_dataset(fi)
             lon = sample_post.longitude-360
             xlon, nlon = lon.max().values, lon.min().values
             sample_post.close()
-            os.remove(f)
-
+            
             if xlon == -100.0 and nlon == -128.0:
-                print(xlon, nlon)
                 time.sleep(5)
-
+                
                 p = Pool(cpu_count()-1)
                 returns = p.map(ncks_subset_mp, range(len(flist)), chunksize=1)
                 p.close()
                 p.join()
-            
+
             else:
                 print('LAT LON SUBSET INCORRECT')
                 raise
 
             # BE CAREFUL HERE!
-            nfiles = 300 if griddtype == 'isobaric' else 60
+            nfiles = 300 if gridtype == 'isobaric' else 60
             flist_check = glob(d + '/*.WE.nc')
-            # if len(flist_check) >= nfiles:
-            if len(flist_check) == len(flist):
-                [os.remove(f) for f in flist]
+            
+            print('check: ', len(flist), len(flist_check))
+            
+            #if len(flist_check) == len(flist):
+            #    [os.remove(f) for f in flist]
